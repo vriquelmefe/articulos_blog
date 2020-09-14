d83954bf4c722fecb9f82832f3fcf0e4d6d6f1a3
Vanessa_Riquelme <vriquelmefe@gmail.com>
2020-09-14 00:40:22 -0300

    active boton borrar en comentarios

--- app/js/eliminar_comentario.js
+++ app/js/eliminar_comentario.js
@@ -2,16 +2,17 @@ $(function() {
     console.log( "ready!" );
 });
 
-$( "#articulo" ).submit(function( event ) {
+$( "#comentario_eliminar" ).submit(function( event ) {
     event.preventDefault();
     // Se pasan a TODAS las variables de los datos del formulario
     const data = $( this ).serialize();
+    console.log(data);
     $.ajax({
         type:"POST", // la variable type guarda el tipo de la peticion GET,POST,..
-        url:"../funciones/eliminar_comentario.php", //url guarda la ruta hacia donde se hace la peticion
+        url:"../funciones/comentario_eliminar.php", //url guarda la ruta hacia donde se hace la peticion
         data: data, // data recibe un objeto con la informacion que se enviara al servidor
         success:function(datos){ //success es una funcion que se utiliza si el servidor retorna informacion
-            window.location.replace("../view/index.php");
+            window.location.replace(`../view/articulo.php?id=${$("#id_articulo").val()}`);
             
         },
          // El tipo de datos esperados del servidor. Valor predeterminado: Intelligent Guess (xml, json, script, text, html).
